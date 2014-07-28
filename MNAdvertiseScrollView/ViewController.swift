028a6d11030480085fd977977adbc2e615dd5bbe
Xiaomuniu <lugq1001@icloud.com>
2014-07-28 15:50:40 +0800

    create project

---
+++ MNAdvertiseScrollView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MNAdvertiseScrollView
+//
+//  Created by 陆广庆 on 14/7/28.
+//  Copyright (c) 2014年 陆广庆. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
