f4ce9575794ed7915936f238917a235e587c0f35
Xiaomuniu <lugq1001@icloud.com>
2014-07-29 14:24:33 +0800

    sync

---
+++ MNAdvertiseScrollView/Core/MNAdvertiseScrollView.swift
@@ -0,0 +1,27 @@
+//
+//  MNAdvertiseScrollView.swift
+//  MNAdvertiseScrollView
+//
+//  Created by 陆广庆 on 14/7/29.
+//  Copyright (c) 2014年 陆广庆. All rights reserved.
+//
+
+import UIKit
+
+class MNAdvertiseScrollView: UIView {
+
+    init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect)
+    {
+        // Drawing code
+    }
+    */
+
+}
